92a93b403e5136b99e76a5a0e38389ccc3b0bd41
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-09-20 13:56:35 +0530

    Update gitinfo.py

--- BABYMUSIC/plugins/tools/gitinfo.py
+++ BABYMUSIC/plugins/tools/gitinfo.py
@@ -17,7 +17,7 @@ async def daxxhub(_, message):
 @app.on_message(filters.command(["github", "git"]))
 async def github(_, message):
     if len(message.command) != 2:
-        await message.reply_text("/git itzshukla")
+        await message.reply_text("/git BABY-MUSIC")
         return
 
     username = message.text.split(None, 1)[1]
