e4b0aa2e17e4c27f53c49b2e57c5296184d2b322
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-10-08 13:37:23 +0530

    Update start.py

--- BABYMUSIC/plugins/bot/start.py
+++ BABYMUSIC/plugins/bot/start.py
@@ -1,6 +1,5 @@
 import time
 import random
-import asyncio  # Add this import
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
@@ -25,6 +24,8 @@ from BABYMUSIC.utils.formatters import get_readable_time
 from BABYMUSIC.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
+
+
 
 YUMI_PICS = [
 "https://graph.org/file/f76fd86d1936d45a63c64.jpg",
@@ -56,112 +57,31 @@ YUMI_PICS = [
 "https://graph.org/file/5846b9214eaf12c3ed100.jpg",
 "https://graph.org/file/ad4f9beb4d526e6615e18.jpg",
 "https://graph.org/file/3514efaabe774e4f181f2.jpg",
+
 ]
 
-# Function to simulate the progress bar animation
-async def send_loading_animation(message: Message):
-    progress_bar = [
-        "□□□□□□□□□□ 0%",
-        "■□□□□□□□□□ 10%",
-        "■■□□□□□□□□ 20%",
-        "■■■□□□□□□□ 30%",
-        "■■■■□□□□□□ 40%",
-        "■■■■■□□□□□ 50%",
-        "■■■■■■□□□□ 60%",
-        "■■■■■■■□□□ 70%",
-        "■■■■■■■■□□ 80%",
-        "■■■■■■■■■□ 90%",
-        "■■■■■■■■■■ 100%"
-    ]
-    
-    for bar in progress_bar:
-        await message.edit(text=bar)
-        await asyncio.sleep(0.5)  # Delay between updates
 
 
-# Modified Start command with filters and animation
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
-    
-    # Start with animation
-    start_msg = await message.reply_text("Starting...")
-    await send_loading_animation(start_msg)  # Show animation
-    await start_msg.delete()  # Delete the message with the loading animation
-    
-    # Check for additional commands (like help)
     if len(message.text.split()) > 1:
         name = message.text.split(None, 1)[1]
-        if name.startswith("help"):
+        if name[0:4] == "help":
             keyboard = help_pannel(_)
             return await message.reply_photo(
                 random.choice(YUMI_PICS),
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
-
-        elif name.startswith("sud"):
+        if name[0:3] == "sud":
             await sudoers_list(client=client, message=message, _=_)
             if await is_on_off(2):
                 return await app.send_message(
                     chat_id=config.LOGGER_ID,
-                    text=f"✦ {message.from_user.mention} just started the bot to check <b>sudolist</b>.\n\n<b>✦ User ID ➠</b> <code>{message.from_user.id}</code>\n<b>✦ Username ➠</b> @{message.from_user.username}",
+                    text=f"✦ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>✦ ᴜsᴇʀ ɪᴅ ➠</b> <code>{message.from_user.id}</code>\n<b>✦ ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.from_user.username}",
                 )
-        
-        elif name.startswith("inf"):
-            m = await message.reply_text("🔎")
-            query = name.replace("info_", "", 1)
-            results = VideosSearch(query, limit=1)
-            for result in (await results.next())["result"]:
-                title = result["title"]
-                duration = result["duration"]
-                views = result["viewCount"]["short"]
-                thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-                channellink = result["channel"]["link"]
-                channel = result["channel"]["name"]
-                link = result["link"]
-                published = result["publishedTime"]
-            
-            searched_text = _["start_6"].format(
-                title, duration, views, published, channellink, channel, app.mention
-            )
-            key = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(text=_["S_B_8"], url=link),
-                        InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT),
-                    ],
-                ]
-            )
-            await m.delete()
-            await app.send_photo(
-                chat_id=message.chat.id,
-                photo=thumbnail,
-                caption=searched_text,
-                reply_markup=key,
-            )
-            if await is_on_off(2):
-                return await app.send_message(
-                    chat_id=config.LOGGER_ID,
-                    text=f"✦ {message.from_user.mention} started the bot to check <b>track information</b>.\n\n✦ <b>User ID ➠</b> <code>{message.from_user.id}</code>\n✦ <b>Username ➠</b> @{message.from_user.username}",
-                )
-    
-    else:
-        out = private_panel(_)
-        served_chats = len(await get_served_chats())
-        served_users = len(await get_served_users())
-        UP, CPU, RAM, DISK = await bot_sys_stats()
-        await message.reply_photo(
-            random.choice(YUMI_PICS),
-            caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM, served_users, served_chats),
-            reply_markup=InlineKeyboardMarkup(out),
-        )
-        if await is_on_off(2):
-            return await app.send_message(
-                chat_id=config.LOGGER_ID,
-                text=f"✦ {message.from_user.mention} just started the bot.\n\n✦ <b>User ID ➠</b> <code>{message.from_user.id}</code>\n✦ <b>Username ➠</b> @{message.from_user.username}",
-            )
             return
         if name[0:3] == "inf":
             m = await message.reply_text("🔎")
@@ -201,28 +121,20 @@ async def start_pm(client, message: Message, _):
                     text=f"✦ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n✦ <b>ᴜsᴇʀ ɪᴅ ➠</b> <code>{message.from_user.id}</code>\n✦ <b>ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.from_user.username}",
                 )
     else:
-    out = private_panel(_)
-    served_chats = len(await get_served_chats())
-    served_users = len(await get_served_users())
-    UP, CPU, RAM, DISK = await bot_sys_stats()
-    
-    await message.reply_photo(
-        random.choice(YUMI_PICS),
-        caption=_["start_2"].format(
-            message.from_user.mention, app.mention, UP, DISK, CPU, RAM, served_users, served_chats
-        ),
-        reply_markup=InlineKeyboardMarkup(out),
-    )
-    
-    if await is_on_off(2):
-        return await app.send_message(
-            chat_id=config.LOGGER_ID,
-            text=(
-                f"✦ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n"
-                f"✦ <b>ᴜsᴇʀ ɪᴅ ➠</b> <code>{message.from_user.id}</code>\n"
-                f"✦ <b>ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.from_user.username}"
-            ),
+        out = private_panel(_)
+        served_chats = len(await get_served_chats())
+        served_users = len(await get_served_users())
+        UP, CPU, RAM, DISK = await bot_sys_stats()
+        await message.reply_photo(
+            random.choice(YUMI_PICS),
+            caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM,served_users,served_chats),
+            reply_markup=InlineKeyboardMarkup(out),
         )
+        if await is_on_off(2):
+            return await app.send_message(
+                chat_id=config.LOGGER_ID,
+                text=f"✦ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n✦ <b>ᴜsᴇʀ ɪᴅ ➠</b> <code>{message.from_user.id}</code>\n✦ <b>ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.from_user.username}",
+            )
 
 
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
@@ -230,13 +142,11 @@ async def start_pm(client, message: Message, _):
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
-    
     await message.reply_photo(
         random.choice(YUMI_PICS),
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
-    
     return await add_served_chat(message.chat.id)
 
 
@@ -246,18 +156,15 @@ async def welcome(client, message: Message):
         try:
             language = await get_lang(message.chat.id)
             _ = get_string(language)
-            
             if await is_banned_user(member.id):
                 try:
                     await message.chat.ban_member(member.id)
                 except:
                     pass
-            
             if member.id == app.id:
                 if message.chat.type != ChatType.SUPERGROUP:
                     await message.reply_text(_["start_4"])
                     return await app.leave_chat(message.chat.id)
-
                 if message.chat.id in await blacklisted_chats():
                     await message.reply_text(
                         _["start_5"].format(
